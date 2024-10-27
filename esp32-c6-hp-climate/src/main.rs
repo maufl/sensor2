172a013ba0fe948b0c7dfb831d7a714fcd7d77b6
Felix Konstantin Maurer <maufl@maufl.de>
2024-10-27 17:09:24 +0100

    Add LP core use as feature, reformat file

--- esp32-c6-hp-climate/src/main.rs
+++ esp32-c6-hp-climate/src/main.rs
@@ -1,4 +1,3 @@
-
 #![no_std]
 #![no_main]
 
@@ -7,7 +6,15 @@ use core::time::Duration;
 use bme280::i2c::BME280;
 use esp_backtrace as _;
 use esp_hal::{
-    clock::ClockControl, delay::Delay, gpio::{lp_io::LowPowerOutputOpenDrain, Io}, i2c::{lp_i2c::LpI2c, I2C}, lp_core::{LpCore, LpCoreWakeupSource}, peripherals::{self, Peripherals}, prelude::*, rtc_cntl::{sleep::TimerWakeupSource, Rtc}, system::SystemControl
+    clock::ClockControl,
+    delay::Delay,
+    gpio::{lp_io::LowPowerOutputOpenDrain, Io},
+    i2c::{lp_i2c::LpI2c, I2C},
+    lp_core::{LpCore, LpCoreWakeupSource},
+    peripherals::Peripherals,
+    prelude::*,
+    rtc_cntl::{sleep::TimerWakeupSource, Rtc},
+    system::SystemControl,
 };
 use esp_println::{print, println};
 
@@ -23,20 +30,30 @@ struct Measurement {
 const SHARED_MEMORY: u32 = 0x5000_2000;
 
 #[entry]
-fn main() -> ! {    
+fn main() -> ! {
     let peripherals = Peripherals::take();
 
-    read_with_hp(peripherals);
+    if cfg!(feature = "lp") {
+        read_with_lp(peripherals)
+    } else {
+        read_with_hp(peripherals);
+    }
 }
 
 fn read_with_hp(peripherals: Peripherals) -> ! {
-    
     let system = SystemControl::new(peripherals.SYSTEM);
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
     let mut delay = Delay::new(&clocks);
     let io = Io::new(peripherals.GPIO, peripherals.IO_MUX);
 
-    let i2c = I2C::new(peripherals.I2C0, io.pins.gpio6, io.pins.gpio7, 100.kHz(), &clocks, None);
+    let i2c = I2C::new(
+        peripherals.I2C0,
+        io.pins.gpio6,
+        io.pins.gpio7,
+        100.kHz(),
+        &clocks,
+        None,
+    );
 
     let mut bme280 = BME280::new_primary(i2c);
     println!("Initializing sensor");
@@ -46,7 +63,10 @@ fn read_with_hp(peripherals: Peripherals) -> ! {
     println!("Finished initializing sensor");
     for _ in 0..10 {
         match bme280.measure(&mut delay) {
-            Ok(m) => print!("Measured t:{} p:{} h:{}       \u{000d}", m.temperature, m.pressure, m.humidity),
+            Ok(m) => print!(
+                "Measured t:{} p:{} h:{}       \u{000d}",
+                m.temperature, m.pressure, m.humidity
+            ),
             Err(e) => println!("Error reading sensor {:?}", e),
         };
         delay.delay_millis(2_000);
@@ -58,12 +78,9 @@ fn read_with_hp(peripherals: Peripherals) -> ! {
     println!("sleeping!");
     delay.delay_millis(100);
     rtc.sleep_deep(&[&timer]);
-
-    loop {}
 }
 
 fn read_with_lp(peripherals: Peripherals) -> ! {
- 
     let system = SystemControl::new(peripherals.SYSTEM);
     let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
     let delay = Delay::new(&clocks);
@@ -79,8 +96,9 @@ fn read_with_lp(peripherals: Peripherals) -> ! {
     println!("lp core stopped");
 
     // load code to LP core
-    let lp_core_code =
-        load_lp_code!("../esp32-c6-lp-climate/target/riscv32imac-unknown-none-elf/release/esp32-c6-lp-climate");
+    let lp_core_code = load_lp_code!(
+        "../esp32-c6-lp-climate/target/riscv32imac-unknown-none-elf/release/esp32-c6-lp-climate"
+    );
 
     // start LP core
     lp_core_code.run(&mut lp_core, LpCoreWakeupSource::HpCpu, lp_i2c);
