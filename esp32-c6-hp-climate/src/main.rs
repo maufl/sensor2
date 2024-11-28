6a37ba070fc03567babc16f6ddd6870c568198e8
Felix Konstantin Maurer <maufl@maufl.de>
2024-11-28 10:01:36 +0100

    wip: Trying to make BLE work

--- esp32-c6-hp-climate/src/main.rs
+++ esp32-c6-hp-climate/src/main.rs
@@ -1,24 +1,28 @@
 #![no_std]
 #![no_main]
 
+use defmt_rtt as _;
+
 use core::time::Duration;
 
+use bleps::{
+    ad_structure::{
+        create_advertising_data, AdStructure, BR_EDR_NOT_SUPPORTED, LE_GENERAL_DISCOVERABLE,
+    },
+    att::Uuid,
+    Ble, HciConnector,
+};
 use bme280::i2c::BME280;
 use esp_backtrace as _;
 use esp_hal::{
-    analog::adc::{Adc, AdcConfig, Attenuation},
-    delay::Delay,
-    gpio::{lp_io::LowPowerOutputOpenDrain, GpioPin},
-    i2c::{
+    analog::adc::{Adc, AdcConfig, Attenuation}, delay::Delay, gpio::{lp_io::LowPowerOutputOpenDrain, GpioPin}, i2c::{
         lp_i2c::LpI2c,
         master::{Config, I2c},
-    },
-    lp_core::{LpCore, LpCoreWakeupSource},
-    peripherals::{Peripherals, ADC1},
-    prelude::*,
-    rtc_cntl::{sleep::TimerWakeupSource, Rtc},
+    }, lp_core::{LpCore, LpCoreWakeupSource}, peripherals::{Peripherals, ADC1, BT, RADIO_CLK, RNG, TIMG0}, prelude::*, rng::Rng, rtc_cntl::{sleep::TimerWakeupSource, Rtc}, timer::timg::TimerGroup
 };
-use esp_println::{print, println};
+
+use defmt::println;
+use esp_wifi::ble::controller::BleConnector;
 
 #[derive(Debug)]
 #[repr(C)]
@@ -33,7 +37,13 @@ const SHARED_MEMORY: u32 = 0x5000_2000;
 
 #[entry]
 fn main() -> ! {
-    let peripherals = esp_hal::init(Default::default());
+    defmt::info!("Starting up");
+    
+    let mut config = esp_hal::Config::default();
+    config.cpu_clock = CpuClock::max();
+    let peripherals = esp_hal::init(config);
+
+    esp_alloc::heap_allocator!(72 * 1024);
 
     if cfg!(feature = "lp") {
         read_with_lp(peripherals)
@@ -50,6 +60,18 @@ fn read_battery_charge(analog_pin: GpioPin<2>, adc: ADC1) -> u16 {
     return nb::block!(adc1.read_oneshot(&mut pin)).unwrap();
 }
 
+fn sent_ble_advertisment(ble: &mut Ble) {
+    let _ = ble.cmd_set_le_advertising_data(
+        create_advertising_data(&[
+            AdStructure::Flags(LE_GENERAL_DISCOVERABLE | BR_EDR_NOT_SUPPORTED),
+            AdStructure::ServiceUuids16(&[Uuid::Uuid16(0xFCD2)]),
+            AdStructure::CompleteLocalName("esp32c6"),
+        ])
+        .unwrap(),
+    );
+    let _ = ble.cmd_set_le_advertise_enable(true);
+}
+
 fn read_with_hp(peripherals: Peripherals) -> ! {
     let Peripherals {
         LPWR,
@@ -58,6 +80,10 @@ fn read_with_hp(peripherals: Peripherals) -> ! {
         GPIO2,
         GPIO6,
         GPIO7,
+        TIMG0,
+        RNG,
+        RADIO_CLK,
+        mut BT,
         ..
     } = peripherals;
     let mut delay = Delay::new();
@@ -73,18 +99,36 @@ fn read_with_hp(peripherals: Peripherals) -> ! {
     .with_scl(GPIO7);
 
     let mut bme280 = BME280::new_primary(i2c);
-    println!("Initializing sensor");
+    defmt::info!("Initializing sensor");
     if let Err(e) = bme280.init(&mut delay) {
         println!("Error initializing sensor {:?}", e)
     };
-    println!("Finished initializing sensor");
+    defmt::info!("Finished initializing sensor");
+
+    println!("Starting setup of Bluetooth stack");
+    let timg0 = TimerGroup::new(TIMG0);
+    println!("Initializing Wifi controller");
+    let wifi_controller = esp_wifi::init(timg0.timer0, Rng::new(RNG), RADIO_CLK)
+       .expect("To initialize Wifi controller");
+    println!("Setting up BLE connector");
+    let connector = BleConnector::new(&wifi_controller, &mut BT);
+    let now = || esp_hal::time::now().duration_since_epoch().to_millis();
+    let hci = HciConnector::new(connector, now);
+    let mut ble = Ble::new(&hci);
+    println!("Finished setup of Bluetooth stack, initializing");
+    println!("{:?}", ble.init());
+    let _ = ble.cmd_set_le_advertising_parameters();
+
     let battery_charge = read_battery_charge(GPIO2, ADC1);
-    for _ in 0..10 {
+    loop {
         match bme280.measure(&mut delay) {
-            Ok(m) => print!(
-                "Measured t:{} p:{} h:{} b:{}      \u{000d}",
-                m.temperature, m.pressure, m.humidity, battery_charge
-            ),
+            Ok(m) => {
+                println!(
+                    "Measured t:{} p:{} h:{} b:{}      \u{000d}",
+                    m.temperature, m.pressure, m.humidity, battery_charge
+                );
+                sent_ble_advertisment(&mut ble);
+            }
             Err(e) => println!("Error reading sensor {:?}", e),
         };
         delay.delay_millis(2_000);
@@ -121,9 +165,11 @@ fn read_with_lp(peripherals: Peripherals) -> ! {
 
     let ptr = SHARED_MEMORY as *mut Measurement;
     loop {
-        print!("Current {:?}           \u{000d}", unsafe {
-            ptr.read_volatile()
-        });
+        let m = unsafe { ptr.read_volatile() };
+        println!(
+            "Measured t:{} p:{} h:{}      \u{000d}",
+            m.temperature, m.pressure, m.humidity
+        );
         delay.delay_millis(1_000);
     }
 }
